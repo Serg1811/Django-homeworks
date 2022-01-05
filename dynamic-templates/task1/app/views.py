38cecacee1ca4ea1b289301e003c125b1a5fec79
Serg <Serg181182@mail.ru>
2022-01-06 02:44:35 +0600

    task1

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,20 @@
+import csv
+
 from django.shortcuts import render
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
 
     # чтение csv-файла и заполнение контекста
-    context = {}
-
-    return render(request, template_name,
-                  context)
+    with open('inflation_russia.csv', encoding='utf=8') as file:
+        reader = csv.reader(file, delimiter=';')
+        headers = []
+        rows = []
+        for i, row in enumerate(reader):
+            if i == 0:
+                headers = row
+            else:
+                rows += [[value if i == 0 or value == '' else float(value) for i, value in enumerate(row)]]
+    context = {'headers': headers, 'rows': rows}
+    return render(request, template_name, context)
